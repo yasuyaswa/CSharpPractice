94de92fed7a25669a53bc64578e0bdac39b5c27b
yaswanth <yasuyaswa@gmail.com>
2023-07-26 16:27:38 +0530

    code simplified

--- String/SortStringArrayInAO.cs
+++ String/SortStringArrayInAO.cs
@@ -1,24 +1,28 @@
-public class SortStringArrayInAO{
-    public static void SortStringArray(){
+public class SortStringArrayInAO
+{
+    public static void SortStringArray()
+    {
         Console.Write("Enter the text: ");
         string input = Console.ReadLine();
-        char[] a;
-        char ch;
-        int l=input.Length;
-        a = input.ToCharArray(0,l);
-        for(int i=1;i<l; i++){
-            for(int j=0; j<l-i; j++){
-                if(a[j]>a[j+1]){
-                    ch=a[j];
-                    a[j]=a[j+1];
-                    a[j+1]=ch;
+        char[] a = input.ToCharArray();
+        int l = a.Length;
+
+        for (int i = 0; i < l - 1; i++)
+        {
+            for (int j = 0; j < l - i - 1; j++)
+            {
+                if (a[j] > a[j + 1])
+                {
+                    char temp = a[j];
+                    a[j] = a[j + 1];
+                    a[j + 1] = temp;
                 }
-                
             }
         }
+
         foreach (var item in a)
-                {
-                    Console.Write("{0} ", item);
-                }
+        {
+            Console.Write(item + " ");
+        }
     }
 }
