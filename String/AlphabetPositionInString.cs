742a772e4ed33aa6daac7f854b6dfea919b61198
yaswanth <yasuyaswa@gmail.com>
2023-12-09 14:18:54 +0530

    code simplified

--- String/AlphabetPositionInString.cs
+++ String/AlphabetPositionInString.cs
@@ -3,7 +3,7 @@ public class AlphabetPositionInString{
         Console.Write("Enter the string: ");
         string input = Console.ReadLine();
         //a -> 97%32=1
-        var CharPosition = input.ToLower().Where(ch=> Char.IsLetter(ch)).Select(ch=> (int)ch%32).ToArray();
+        var CharPosition = input.ToLower().Where(Char.IsLetter).Select(ch=> ch-'a'+1);
         string result = string.Join(" ", CharPosition);
         Console.WriteLine("Alphabet position in the string is "+ result);
     }
